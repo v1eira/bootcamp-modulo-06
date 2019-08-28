7bb146d0fbcc863f88a9e6e2717fc52a729aa2bd
v1eira <ewersonv@gmail.com>
2019-08-27 21:25:29 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import './config/ReactotronConfig';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+console.tron.log('Hello world');
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+    </View>
+  );
+};
+
+export default App;
