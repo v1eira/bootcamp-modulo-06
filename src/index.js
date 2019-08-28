c7a3953692ea54f7b0a3a92ad4ff5cf748791459
v1eira <ewersonv@gmail.com>
2019-08-27 23:35:43 -0300

    end Estilizando formulário

--- src/index.js
+++ src/index.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-console.tron.log('Hello world');
+import Routes from './routes';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
   );
 };
 
---
+++ src/pages/Main/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Add user"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
+
+Main.navigationOptions = {
+  title: 'Users',
+};
